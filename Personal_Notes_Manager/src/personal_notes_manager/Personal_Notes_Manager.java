17943997c4576e545cb0b482280e96d05e148404
Niham1006 <nihamalamgir21@gmail.com>
2025-06-18 00:05:42 +0600

    Week 1, Attempt 1
    Project Create & Login page outlook + signup FXML file create

---
+++ Personal_Notes_Manager/src/personal_notes_manager/Personal_Notes_Manager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package personal_notes_manager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *      Signup & Login
+ * @author Niham
+ */
+public class Personal_Notes_Manager extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
